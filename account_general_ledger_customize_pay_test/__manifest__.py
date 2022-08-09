2f1ec6e2b94ca29fcc363d954c705cbb5a2b96dc
ksharry <ksharry1025@gmail.com>
2022-08-09 11:07:56 +0800

    [ADD]

---
+++ account_general_ledger_customize_pay_test/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'account_general_ledger_customize_pay_test',
+    'version': '13.0.1.0.0',
+    'summary': '總分類帳串查與餘額查詢',
+    'sequence': -100,
+    'description': """總分類帳串查與餘額查詢""",
+    'category': 'Accounting/Accounting',
+    'author': 'Harry Chang',
+    'maintainer': 'Harry Chang',
+    'price': '10.0',
+    'currency': 'USD',
+    'license': 'LGPL-3',
+    'depends': ['base', 'account'],
+    'data': [
+        'views/account_move.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'images': ['static/description/banner.png'],
+    'intallable': True,
+    'application': True,
+    'auto_install': False,
+}
